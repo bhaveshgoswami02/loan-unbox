b0ad6e111d61fba4ee5f3515ea640af0e1fc46c1
thepresencedotin <splixcube@gmail.com>
2021-10-24 16:30:14 +0530

    changes

--- admin/src/app/components/users/users.component.ts
+++ admin/src/app/components/users/users.component.ts
@@ -15,13 +15,55 @@ export class UsersComponent implements OnInit {
     { field: 'mobile_no', header: 'Mobile' },
     { field: 'gender', header: 'Gender' },
     { field: 'timestamp', header: 'Date' },
+    { field: 'address_details', header: 'Address' },
     { field: 'notification', header: 'Notification' },
   ];
   loading:boolean = true;
+  allStates: any = []
+  state: any = {name:"All"}
 
   constructor(public user: UserService, public router: Router) { }
 
   ngOnInit(): void {
+    this.allStates = [
+      {name:"All"},
+      {name:"Andaman and Nicobar Islands"},
+      {name:"Andhra Pradesh"},
+      {name:"Arunachal Pradesh"},
+      {name:"Assam"},
+      {name:"Bihar"},
+      {name:"Chandigarh"},
+      {name:"Chhattisgarh"},
+      {name:"Dadra and Nagar Haveli"},
+      {name:"Daman and Diu"},
+      {name:"Delhi"},
+      {name:"Goa"},
+      {name:"Gujarat"},
+      {name:"Haryana"},
+      {name:"Himachal Pradesh"},
+      {name:"Jammu and Kashmir"},
+      {name:"Jharkhand"},
+      {name:"Karnataka"},
+      {name:"Kerala"},
+      {name:"Lakshadweep"},
+      {name:"Madhya Pradesh"},
+      {name:"Maharashtra"},
+      {name:"Manipur"},
+      {name:"Meghalaya"},
+      {name:"Mizoram"},
+      {name:"Nagaland"},
+      {name:"Odisha"},
+      {name:"Puducherry"},
+      {name:"Punjab"},
+      {name:"Rajasthan"},
+      {name:"Sikkim"},
+      {name:"Tamil Nadu"},
+      {name:"Telangana"},
+      {name:"Tripura"},
+      {name:"Uttar Pradesh"},
+      {name:"Uttarakhand"},
+      {name:"West Bengal"}
+    ]
     this.getData()
   }
 
@@ -30,11 +72,15 @@ export class UsersComponent implements OnInit {
   }
 
   getData() {
-    this.user.getAll().subscribe(res => {
+    this.user.getAll(this.state.name).subscribe(res => {
       this.allData = res
       this.loading = false
       console.log("all users", this.allData)
     })
+  }
+
+  onStateChange() {
+    this.getData()
   }
 
 }
--- admin/src/app/services/user.service.ts
+++ admin/src/app/services/user.service.ts
@@ -12,14 +12,26 @@ export class UserService {
 
   constructor(public db: AngularFirestore, public router: Router, public common: CommonService) { }
 
-  getAll() {
-    return this.db.collection(this.collection).get().pipe(
-      map(actions => actions.docs.map(a => {
-        const data = a.data() as any;
-        const id = a.id;
-        return { id, ...data };
-      }))
-    )
+  getAll(state:string) {
+    if(state == 'All') {
+      return this.db.collection(this.collection,ref=>ref.orderBy("timestamp","desc")).get().pipe(
+        map(actions => actions.docs.map(a => {
+          const data = a.data() as any;
+          const id = a.id;
+          return { id, ...data };
+        }))
+      )
+    }
+    else
+    {
+      return this.db.collection(this.collection,ref=>ref.where("address_details.state","==",state).orderBy("timestamp","desc")).get().pipe(
+        map(actions => actions.docs.map(a => {
+          const data = a.data() as any;
+          const id = a.id;
+          return { id, ...data };
+        }))
+      )
+    }
   }
 
 }

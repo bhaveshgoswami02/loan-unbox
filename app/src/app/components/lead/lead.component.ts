0492039ab3ac20558539e654743f35dea428a205
thepresencedotin <splixcube@gmail.com>
2021-10-24 16:30:30 +0530

    changes

--- app/src/app/components/lead/lead.component.ts
+++ app/src/app/components/lead/lead.component.ts
@@ -10,8 +10,47 @@ import { LeadService } from 'src/app/services/lead.service';
 })
 export class LeadComponent implements OnInit {
   formData: FormGroup;
+  allStates: any = []
 
   constructor(private fb: FormBuilder, public service: LeadService, public auth: AuthService) {
+    this.allStates = [
+      { name: "Andaman and Nicobar Islands" },
+      { name: "Andhra Pradesh" },
+      { name: "Arunachal Pradesh" },
+      { name: "Assam" },
+      { name: "Bihar" },
+      { name: "Chandigarh" },
+      { name: "Chhattisgarh" },
+      { name: "Dadra and Nagar Haveli" },
+      { name: "Daman and Diu" },
+      { name: "Delhi" },
+      { name: "Goa" },
+      { name: "Gujarat" },
+      { name: "Haryana" },
+      { name: "Himachal Pradesh" },
+      { name: "Jammu and Kashmir" },
+      { name: "Jharkhand" },
+      { name: "Karnataka" },
+      { name: "Kerala" },
+      { name: "Lakshadweep" },
+      { name: "Madhya Pradesh" },
+      { name: "Maharashtra" },
+      { name: "Manipur" },
+      { name: "Meghalaya" },
+      { name: "Mizoram" },
+      { name: "Nagaland" },
+      { name: "Odisha" },
+      { name: "Puducherry" },
+      { name: "Punjab" },
+      { name: "Rajasthan" },
+      { name: "Sikkim" },
+      { name: "Tamil Nadu" },
+      { name: "Telangana" },
+      { name: "Tripura" },
+      { name: "Uttar Pradesh" },
+      { name: "Uttarakhand" },
+      { name: "West Bengal" }
+    ]
     this.formData = this.fb.group({
       'prefix': ['mr.', [Validators.required]],
       'firstName': ['', [Validators.required]],
